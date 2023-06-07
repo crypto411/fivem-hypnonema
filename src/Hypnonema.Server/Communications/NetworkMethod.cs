08e144b9c25a5035ee7023938f485777215608c8
Fadhlan Hadaina <fadhlan.hadaina@gmail.com>
2023-06-07 17:39:58 +0700

    listen S2C events and broadcast S2S with target

--- src/Hypnonema.Server/Communications/NetworkMethod.cs
+++ src/Hypnonema.Server/Communications/NetworkMethod.cs
@@ -72,6 +72,7 @@
                 BaseScript.TriggerClientEvent(target, "hyp:S2C:" + this.EventName, args);
             else
                 BaseScript.TriggerClientEvent("hyp:S2C:" + this.EventName, args);
+            BaseScript.TriggerEvent("hyp:S2S:" + this.EventName, args, target);
         }
     }
 
